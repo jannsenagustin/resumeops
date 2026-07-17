6320ec8fdc69c9215ef9d4d83ed48864aa27020e
Jannsen Agustin <jannsenagustin.can@gmail.com>
2026-07-16 23:00:14 -0600

    updated next.config.ts to connect to gitpages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProduction ? "/resumeops" : "",
+  assetPrefix: isProduction ? "/resumeops/" : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
